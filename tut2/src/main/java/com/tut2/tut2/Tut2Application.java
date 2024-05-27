2d40aea657c5c6f11bf634f0761c1ff677581824
rahulpro1012 <rahulmudaliar1012@gmail.com>
2024-05-27 10:45:10 +0530

    Initial commit

---
+++ tut2/src/main/java/com/tut2/tut2/Tut2Application.java
@@ -0,0 +1,13 @@
+package com.tut2.tut2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tut2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tut2Application.class, args);
+	}
+
+}
---
+++ tut2/src/test/java/com/tut2/tut2/Tut2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tut2.tut2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tut2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
